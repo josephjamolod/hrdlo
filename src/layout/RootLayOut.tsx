8b0ac7e162722eeb4db93d288666f4aa7a8771a0
Joseph Jamolod <josephjamolod@gmail.com>
2025-02-22 11:49:20 +0800

    fix: adjust ui

--- src/layout/RootLayOut.tsx
+++ src/layout/RootLayOut.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 import Navigation from "../components/navigation";
+import styles from "../styles/RootLayout.module.css";
 
 export default function RootLayout() {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       <Navigation />
       <Outlet />
-    </>
+    </div>
   );
 }
