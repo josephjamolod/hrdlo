8f73c6b6ad017559e26ab1afddb53eccaa5952c5
Joseph Jamolod <josephjamolod@gmail.com>
2025-02-22 12:43:58 +0800

    feat: add isActive styling to navs

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,22 +1,42 @@
+import { Link, useLocation } from "react-router-dom";
 import { navItems } from "../../constant/navigation";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
+  const location = useLocation();
+
   return (
     <nav className={styles.navigation}>
-      {navItems.map((item, index) => (
-        <div key={index} className={styles.navItemWrapper}>
-          <button className={styles.navItem}>
-            <img src={item.src} alt={item.label} className={styles.icon} />
-            <span className={styles.label}>{item.label}</span>
-          </button>
-          {index < navItems.length - 1 && (
-            <div className={styles.separatorWrapper}>
-              <hr className={styles.separator} />
-            </div>
-          )}
-        </div>
-      ))}
+      {navItems.map((item, index) => {
+        const isActive = location.pathname === item.href;
+        return (
+          <Link to={item.href} key={index} className={styles.navItemWrapper}>
+            <button
+              className={`${styles.navItem} ${isActive ? styles.active : ""}`}
+            >
+              <img
+                src={item.src}
+                alt={item.label}
+                className={`${styles.icon} ${
+                  isActive ? styles.activeIcon : ""
+                }`}
+              />
+              <span
+                className={`${styles.label} ${
+                  isActive ? styles.activeLabel : ""
+                }`}
+              >
+                {item.label}
+              </span>
+            </button>
+            {index < navItems.length - 1 && (
+              <div className={styles.separatorWrapper}>
+                <hr className={styles.separator} />
+              </div>
+            )}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
--- src/constant/navigation/index.ts
+++ src/constant/navigation/index.ts
@@ -6,10 +6,14 @@ import attendanceConsulting from "../../assets/svg/navigation/consultation.svg";
 import trainee from "../../assets/svg/navigation/trainee.svg";
 
 export const navItems = [
-  { src: attendanceStatus, label: "출강신청" },
-  { src: trainee, label: "훈련생 등록" },
-  { src: attendanceSheet, label: "출석부" },
-  { src: attendanceTimeTable, label: "시간표" },
-  { src: attendanceConsulting, label: "상담관리" },
-  { src: attendanceCBT, label: "CBT (문제은행)" },
+  { src: attendanceStatus, label: "출강신청", href: "/" },
+  { src: trainee, label: "훈련생 등록", href: "/trainee-registration" },
+  { src: attendanceSheet, label: "출석부", href: "/attendance-sheet" },
+  { src: attendanceTimeTable, label: "시간표", href: "/time-table" },
+  {
+    src: attendanceConsulting,
+    label: "상담관리",
+    href: "/consultation-management",
+  },
+  { src: attendanceCBT, label: "CBT (문제은행)", href: "/CBT" },
 ];
