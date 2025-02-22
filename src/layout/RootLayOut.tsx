24a39c1c34483724f737774fcd37317465e26a49
Joseph Jamolod <josephjamolod@gmail.com>
2025-02-22 20:56:41 +0800

    fix: fix detail page ui

--- src/layout/RootLayOut.tsx
+++ src/layout/RootLayOut.tsx
@@ -1,13 +1,21 @@
+import { useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 import Navigation from "../components/navigation";
 import styles from "../styles/RootLayout.module.css";
 
 export default function RootLayout() {
+  const { pathname } = useLocation();
+  // Check if the path matches "/faq/{id}" or "/announcements/{id}"
+  const isDetailsPage = /^\/(faq|announcements)\/[^/]+$/.test(pathname);
+
+  // console.log("Is Details Page:", isDetailsPage);
+
   return (
     <div className={styles.container}>
       <Header />
-      <Navigation />
+      {/* Show navigation only if it's NOT a details page */}
+      {!isDetailsPage && <Navigation />}
       <Outlet />
     </div>
   );
