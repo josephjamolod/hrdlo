19886076478395c28026e00eecccb0369bc95c01
Joseph Jamolod <josephjamolod@gmail.com>
2025-02-22 13:24:08 +0800

    feat: complete trainee-registration page

---
+++ src/pages/TraineeRegistration.tsx
@@ -0,0 +1,88 @@
+import styles from "../styles/TraineeRegistration.module.css";
+import SearchInput from "../components/searchInput";
+import trainee from "../assets/svg/trainee-registration/trainee.svg";
+import talk from "../assets/svg/trainee-registration/talk.svg";
+import sms from "../assets/svg/trainee-registration/SMS.svg";
+import sendMail from "../assets/svg/trainee-registration/send_mail.svg";
+
+export default function TraineeRegistration() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.controls}>
+        <div className={styles.buttonContainer}>
+          <div className={styles.buttonGroup}>
+            <SearchInput placeholder="훈련생을 검색하세요." />
+            <button className={styles.redButton}>
+              <img src={trainee} width={20} height={20} alt="훈련생 등록" />
+              훈련생 등록
+            </button>
+          </div>
+          <div className={styles.buttonGroup}>
+            <button className={styles.orangeButton}>
+              문자 발송
+              <img src={sms} width={20} height={20} alt="문자 발송" />
+            </button>
+            <button className={styles.lightOrangeButton}>
+              톡 발송
+              <img src={talk} width={20} height={20} alt="이메일 발송" />
+            </button>
+            <button className={styles.lightestOrangeButton}>
+              이메일 발송
+              <img src={sendMail} width={20} height={20} alt="아이디 발급" />
+            </button>
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <button className={styles.grayButton}>훈련생 삭제</button>
+        </div>
+      </div>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>NO</th>
+            <th>이름</th>
+            <th>아이디</th>
+            <th>생년월일</th>
+            <th>전화번호</th>
+            <th>교육과정</th>
+            <th>수정</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[...Array(12)].map((_, index) => (
+            <tr key={index}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>1</td>
+              <td>홍길동</td>
+              <td>아이디</td>
+              <td>생년월일</td>
+              <td>010-0000-0000</td>
+              <td>구분</td>
+              <td>
+                <button className={styles.editButton}>수정</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className={styles.pagination}>
+        <button>←</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>→</button>
+      </div>
+    </div>
+  );
+}
